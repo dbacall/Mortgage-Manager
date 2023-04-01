d5c9bf7084805400cc8a390335bd0c12843dd00d
dbacall <dbacall@hotmail.co.uk>
2023-04-01 13:52:21 +0100

    add placeholder nav options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,9 @@ import Head from "next/head";
 import { type ReactElement, useState, useEffect } from "react";
 import { SignedInLayout } from "src/components";
 import type { NextPageWithLayout } from './_app';
-import { BiRefresh, BiHelpCircle } from 'react-icons/bi';
+import { BiRefresh, BiCalculator } from 'react-icons/bi';
 import { IoHelpSharp } from 'react-icons/io5';
+import { BsPersonAdd } from 'react-icons/bs';
 
 const Home: NextPageWithLayout = ({ user, clients }) => {
   const [filteredClients, setFilteredClients] = useState(clients)
@@ -79,8 +80,10 @@ const Home: NextPageWithLayout = ({ user, clients }) => {
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu p-4 bg-slate-100 text-base-content w-64">
-            <li className="text-slate-500"><a><BiRefresh size={20} /> Client Renewals</a></li>
-            <li className="text-slate-500"><a><IoHelpSharp size={20} /> Help</a></li>
+            <li className="text-primary-focus"><a><BiRefresh size={20} /> Client Renewals</a></li>
+            <li className="text-slate-400"><a><BsPersonAdd size={20} /> Add Client</a></li>
+            <li className="text-slate-400"><a><BiCalculator size={20} /> Mortgage Calculator</a></li>
+            <li className="text-slate-400"><a><IoHelpSharp size={20} /> Help</a></li>
           </ul>
 
         </div>
