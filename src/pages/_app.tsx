cd818d1342716be6a7aa20d46249987e45b6c95d
dbacall <dbacall@hotmail.co.uk>
2023-04-09 16:13:38 +0100

    add route change loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from 'react-query'
 import "../styles/globals.css";
+import { useRouteLoading } from 'src/hooks';
+import PropagateLoader from "react-spinners/PropagateLoader"
 
 const queryClient = new QueryClient();
 
@@ -13,12 +15,22 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppPropsWithLayout) => {
+  const routeLoading = useRouteLoading()
+
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        {routeLoading ? (
+          <div className="h-full flex justify-center items-center">
+            <PropagateLoader
+              color="#0891b2"
+            />
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </SessionProvider>
     </QueryClientProvider>
   );
